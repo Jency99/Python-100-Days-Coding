0b58d1b00f67b366c1acca426354df5a40725b00
Jency Magdalene <jency.magdalene@xebia.com>
2025-02-10 20:43:55 +0530

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+# This is my first project in this course ----- so happy
+print("Welcome to band name generator")
+user_city = input("Which city did you grew up in?\n")
+user_pet_name = input("What is your pet name?\n")
+print("Your band name could be :" + user_city + " " + user_pet_name )
